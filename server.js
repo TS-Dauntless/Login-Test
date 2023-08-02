aa257aee21d57e9942b37360ce3b90b5c325d199
Thamarai Selvan <108919779+TS-Dauntless@users.noreply.github.com>
2023-08-02 21:18:35 +0530

    Update1 server.js

--- server.js
+++ server.js
@@ -20,29 +20,24 @@ app.use(bodyParser.json());
 
 app.post("/login", (req, res) => {
     const loginData = req.body;
-    const clientIp = req.ip;
     const responseData = { success: "No" };
     // Checking Username with password
     const query = `select data from users where username="${loginData.username}" and password="${loginData.password}";`;
     pool.query(query, (err, result) => {
         if (err) {
-            console.log(displayInfo(clientIp) + "DataBase Query Error: " + err);
+            console.log("DataBase Query Error: " + err);
             res.json(responseData);
         } else {
             if (result.length == 0) {
-                console.log(
-                    displayInfo(clientIp) + "Username Password Mismatch"
-                );
+                console.log("Username Password Mismatch");
                 responseData["success"] = "Mismatch";
                 res.json(responseData);
             } else if (result.length != 1) {
-                console.log(
-                    displayInfo(clientIp) + "Something went wrong in database"
-                );
+                console.log("Something went wrong in database");
                 responseData["success"] = "DataBase Error";
                 res.json(responseData);
             } else {
-                console.log(displayInfo(clientIp) + "Success");
+                console.log("Success");
                 responseData["message"] = result[0].data;
                 responseData["success"] = "Yes";
                 res.json(responseData);
@@ -57,21 +52,5 @@ app.get("/", (req, res) => {
 
 const PORT = 3002;
 app.listen(PORT, () => {
-    console.log(displayInfo("") + "server Started at " + PORT);
+    console.log("server Started at " + PORT);
 });
-
-// Others
-
-function displayInfo(ip) {
-    const today = new Date();
-    const date =
-        today.getFullYear() +
-        "-" +
-        (today.getMonth() + 1) +
-        "-" +
-        today.getDate();
-    const time =
-        today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-
-    return date + " " + time + " " + ip + "  ";
-}
